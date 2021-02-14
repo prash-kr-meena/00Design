eb4f5c1f923f80d7d4581572bcebb69213abd276
prash-kr-meena <social.prash@gmail.com>
2021-02-15 00:31:45 +0530

    adding the LLD module

---
+++ LLD/src/main/java/org/meena/LowLevelDesign.java
@@ -0,0 +1,10 @@
+package org.meena;
+
+/**
+ * Hello world!
+ */
+public class LowLevelDesign {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ LLD/src/test/java/org/meena/LowLevelDesignTest.java
@@ -0,0 +1,19 @@
+package org.meena;
+
+import static org.junit.Assert.assertTrue;
+
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class LowLevelDesignTest {
+  /**
+   * Rigorous Test :-)
+   */
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
